cb85c43ebd110812167d50c1c27a3f521938ce87
Krum Yakimov <krum.yakimov@gmail.com>
2024-10-27 10:01:40 +0200

    feat(models): add post model and refactor author model

--- FurryFunniesApp/authors/models.py
+++ FurryFunniesApp/authors/models.py
@@ -3,24 +3,48 @@ from .validators import LettersOnlyValidator, PasscodeValidator  # Adjust the im
 
 
 class Author(models.Model):
+    MAX_FIRST_NAME_LENGTH = 40
+    MAX_LAST_NAME_LENGTH = 40
+    MAX_PASSCODE_LENGTH = 6
+
     first_name = models.CharField(
-        max_length=40,
+        max_length=MAX_FIRST_NAME_LENGTH,
         validators=[LettersOnlyValidator()],
         help_text="Enter your first name (letters only).",
+        blank=False,
+        null=False,
     )
+
     last_name = models.CharField(
-        max_length=50,
+        max_length=MAX_LAST_NAME_LENGTH,
         validators=[LettersOnlyValidator()],
         help_text="Enter your last name (letters only).",
+        blank=False,
+        null=False,
     )
+
     passcode = models.CharField(
-        max_length=6,
+        max_length=MAX_PASSCODE_LENGTH,
         validators=[PasscodeValidator()],
         help_text="Your passcode must be a combination of 6 digits.",
+        blank=False,
+        null=False,
     )
-    pets_number = models.PositiveSmallIntegerField()
-    info = models.TextField(blank=True, null=True)
-    image_url = models.URLField(blank=True, null=True)
+
+    pets_number = models.PositiveSmallIntegerField(
+        blank=False,
+        null=False,
+    )
+
+    info = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    image_url = models.URLField(
+        blank=True,
+        null=True
+    )
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
--- FurryFunniesApp/posts/models.py
+++ FurryFunniesApp/posts/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.utils import timezone
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+class Post(models.Model):
+    MAX_TITLE_LENGTH = 50
+
+    title = models.CharField(
+        max_length=MAX_TITLE_LENGTH,
+        unique=True,
+        error_messages={
+            'unique': "Oops! That title is already taken. How about something fresh and fun?"
+        },
+        null=False,
+        blank=False,
+    )
+
+    image_url = models.URLField(
+        help_text="Share your funniest furry photo URL!",
+        null=False,
+        blank=False,
+    )
+
+    content = models.TextField(
+        null=False,
+        blank=False,
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        editable=False,
+        null=False,
+        blank=False,
+
+    )
+
+    author = models.ForeignKey(
+        "authors.Author",
+        on_delete=models.CASCADE,
+        related_name='posts',
+        editable=False,
+        blank=False,
+        null=False,
+
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-updated_at']
+
